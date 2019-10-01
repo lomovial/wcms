b6913f9cd1286a0ae4cb4b1b9c77e2043412e17b
valm <lm-va@hotmail.com>
2019-10-01 21:10:12 +0200

    Master commit 2

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
-<?php
-echo "Hola mundo";
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>WCMS</title>
+</head>
+
+<body>
+    <?php
+    echo "Hola mundo";
+    ?>
+</body>
+
+</html>
